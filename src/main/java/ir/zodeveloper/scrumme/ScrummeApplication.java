8f3c9a816c835d649ec5ce73b70369f302d3a2bc
Ali Faraji <ali@01d.ir>
2020-11-18 17:04:32 +0330

    init empty project

---
+++ src/main/java/ir/zodeveloper/scrumme/ScrummeApplication.java
@@ -0,0 +1,13 @@
+package ir.zodeveloper.scrumme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrummeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrummeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/zodeveloper/scrumme/ScrummeApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.zodeveloper.scrumme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrummeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
